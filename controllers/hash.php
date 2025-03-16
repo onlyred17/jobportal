2a62585a0061198569b82cb328f96e381641331e
Jared Vicente <jaredsonvicente1771@gmail.com>
2025-03-16 15:38:38 +0800

    deleted

--- controllers/hash.php
+++
@@ -1,5 +0,0 @@
-<?php
-$password = 'staff1@2025';
-$hashed_password = password_hash($password, PASSWORD_BCRYPT);
-echo $hashed_password;
-?>
--- echo.php
+++
@@ -1,1 +0,0 @@
-<img src="<?php echo !empty($pwd['proof_of_pwd']) ? 'uploads/' . htmlspecialchars($pwd['proof_of_pwd']) : '../images/default.png'; ?>" class="img-fluid" alt="Proof of PWD">
