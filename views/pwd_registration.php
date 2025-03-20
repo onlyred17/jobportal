bdea2a899dbeb946769158ce01eb575de27ea453
Jared Vicente <jaredsonvicente1771@gmail.com>
2025-03-20 12:11:55 +0800

    etc

--- views/pwd_registration.php
+++ views/pwd_registration.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PWD ID Registration</title>
 </head>
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&family=Open+Sans:wght@300;400;600&display=swap" rel="stylesheet">
